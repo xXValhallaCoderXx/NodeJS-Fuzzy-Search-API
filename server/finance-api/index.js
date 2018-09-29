a6fb10468f6fc04cf7d6ec0770186abc971bde32
Renate Gouveia <renate@horangi.com>
2018-09-29 22:02:59 +0800

    Initial jest setup

--- server/finance-api/index.js
+++ server/finance-api/index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 const bodyParser = require("body-parser");
-const { ObjectID } = require("mongodb");
 const {convertCSV} = require("./csv-parser");
+
+const {searchQueryParser} = require("./utils");
 
 router.use(bodyParser.json());
 
 router.get("/people-like-you", (req, res) => {
-  convertCSV(path.resolve(__dirname, "./data.csv")).then(data => {
-    return res.status(200).send({ success: true, data });
-  }).catch(err => {
-    console.log("ERROR: ", err);
-    return res.status(200).send({ success: false, data: null });
-  })
-  
+  // convertCSV(path.resolve(__dirname, "./data.csv")).then(data => {
+  //   return res.status(200).send({ success: true, data });
+  // }).catch(err => {
+  //   console.log("ERROR: ", err);
+  //   return res.status(200).send({ success: false, data: null });
+  // })
+  const searchTerms = searchQueryParser(req.query);
+  console.log("SEARCHTERMS: ", searchTerms);
+  return res.status(200).send({ success: true, data: null });
 });
 
 
