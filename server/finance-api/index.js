9b2398897e16db67d81aded3fd64076aa006da8a
Renate Gouveia <renate@horangi.com>
2018-10-02 02:27:21 +0800

    Test redeploymnet

--- server/finance-api/index.js
+++ server/finance-api/index.js
@@ -3,43 +3,21 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 
 const { multiQuery } = require("./search-queries");
+const { checkParams } = require("./utils");
 
 router.use(bodyParser.json());
 
-let whitelist = [
-  "age",
-  "latitude",
-  "longitude",
-  "monthlyIncome",
-  "experienced"
-];
 
 router.get("/people-like-you", async (req, res) => {
-  if (Object.keys(req.query).length === 0) {
-    return res.status(200).send({
-      success: false,
-      data: `Query params: "age", "latitude", "longitude", "monthlyIncome", "experienced"`
-    });
-  }
   let result = await checkParams(req.query);
   if (!result[0]) {
     return res.status(200).send({
       success: false,
-      data: `Query params: "age", "latitude", "longitude", "monthlyIncome", "experienced"`
+      data: `Query params: 'age', 'latitude', 'longitude', 'monthlyIncome', 'experienced'`
     });
   }
   var nameRes = await multiQuery(req.query, req.query.offset);
   return res.status(200).send({ success: true, data: nameRes });
 });
-
-const checkParams = params => {
-  return Object.keys(params).map(param => {
-    let isIncluded = whitelist.includes(param);
-    if (isIncluded) {
-      return true;
-    }
-    return false;
-  });
-};
 
 module.exports = router;
